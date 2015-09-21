85a5d8ac991cbc3a03edcc95369dc76829153d5b
Andrew Khaynus <a.khaynus@gmail.com>
2015-09-21 18:30:50 +0300

    rename variables

--- app/src/main/java/com/radomar/converterlab/adapters/CurrenciesBaseAdapter.java
+++ app/src/main/java/com/radomar/converterlab/adapters/CurrenciesBaseAdapter.java
@@ -21,12 +21,12 @@ public class CurrenciesBaseAdapter extends BaseAdapter {
     private Context mContext;
     private ArrayList<CurrencyInfoItemModel> mData;
     private LayoutInflater mInflater;
-//TODO rename variables
-    private TextView currencyName;
-    private TextView askCost;
-    private TextView bidCost;
-    private ImageView askMarker;
-    private ImageView bidMarker;
+
+    private TextView mCurrencyName;
+    private TextView mAskCost;
+    private TextView mBidCost;
+    private ImageView mAskMarker;
+    private ImageView mBidMarker;
 
     public CurrenciesBaseAdapter(Context _context, ArrayList<CurrencyInfoItemModel> _data ) {
         this.mContext = _context;
@@ -65,35 +65,35 @@ public class CurrenciesBaseAdapter extends BaseAdapter {
     }
 
     private void findViews(View _view) {
-        currencyName = (TextView) _view.findViewById(R.id.tvCurrency_CR);
-        askCost      = (TextView) _view.findViewById(R.id.tvCost_CR);
-        bidCost      = (TextView) _view.findViewById(R.id.tvCost2_CR);
+        mCurrencyName = (TextView) _view.findViewById(R.id.tvCurrency_CR);
+        mAskCost      = (TextView) _view.findViewById(R.id.tvCost_CR);
+        mBidCost      = (TextView) _view.findViewById(R.id.tvCost2_CR);
 
-        askMarker    = (ImageView) _view.findViewById(R.id.ivMarker_CR);
-        bidMarker    = (ImageView) _view.findViewById(R.id.ivMarker2_CR);
+        mAskMarker    = (ImageView) _view.findViewById(R.id.ivMarker_CR);
+        mBidMarker    = (ImageView) _view.findViewById(R.id.ivMarker2_CR);
     }
 
     private void initViews(int _position) {
         CurrencyInfoItemModel currencyInfoItem = mData.get(_position);
 
-        currencyName.setText(currencyInfoItem.currencyName);
-        askCost.setText(currencyInfoItem.ask);
-        bidCost.setText(currencyInfoItem.bid);
+        mCurrencyName.setText(currencyInfoItem.currencyName);
+        mAskCost.setText(currencyInfoItem.ask);
+        mBidCost.setText(currencyInfoItem.bid);
 
         if (currencyInfoItem.askFlag.equals("1")) {
-            askMarker.setImageResource(R.drawable.ic_green_arrow_up);
-            askCost.setTextColor(mContext.getResources().getColor(R.color.green_arrow_up_color));
+            mAskMarker.setImageResource(R.drawable.ic_green_arrow_up);
+            mAskCost.setTextColor(mContext.getResources().getColor(R.color.green_arrow_up_color));
         } else {
-            askMarker.setImageResource(R.drawable.ic_red_arrow_down);
-            askCost.setTextColor(mContext.getResources().getColor(R.color.red_arrow_down_color));
+            mAskMarker.setImageResource(R.drawable.ic_red_arrow_down);
+            mAskCost.setTextColor(mContext.getResources().getColor(R.color.red_arrow_down_color));
         }
 
         if (currencyInfoItem.bidFlag.equals("1")) {
-            bidMarker.setImageResource(R.drawable.ic_green_arrow_up);
-            bidCost.setTextColor(mContext.getResources().getColor(R.color.green_arrow_up_color));
+            mBidMarker.setImageResource(R.drawable.ic_green_arrow_up);
+            mBidCost.setTextColor(mContext.getResources().getColor(R.color.green_arrow_up_color));
         } else {
-            bidMarker.setImageResource(R.drawable.ic_red_arrow_down);
-            bidCost.setTextColor(mContext.getResources().getColor(R.color.red_arrow_down_color));
+            mBidMarker.setImageResource(R.drawable.ic_red_arrow_down);
+            mBidCost.setTextColor(mContext.getResources().getColor(R.color.red_arrow_down_color));
         }
     }
 }
